4e222082ed41a55ebc26d1cf365913a5d1a6c79e
Jesse Vander Does <jesse@htmlfusion.com>
2015-12-15 20:43:18 -0800

    Adding sample twitter key config

--- twitter-bot/index.js
+++ twitter-bot/index.js
@@ -1,1 +1,9 @@
-console.log("started");
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_TOKEN_SECRET
+});
+
