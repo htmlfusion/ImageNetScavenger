818f493b5698de3a58dc3997a12257e24cb1073c
Jesse Vander Does <jesse@htmlfusion.com>
2015-12-16 21:04:55 -0800

    Added handlers for hunt outcomes

--- twitter-bot/index.js
+++ twitter-bot/index.js
@@ -8,15 +8,39 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_TOKEN_SECRET
 });
 
+function tweetFound(user, match) {
+	var message = '🌟🌟🌟 @' + user.screen_name + ' you found a ' + match + ". We're crossing it off the list http://mmmbrain.com";
+  client.post('statuses/update', {status: message},  function(error, tweet, response){
+  });
+}
+
+function noMatch(user, closest) {
+	var message = '💣💣💣 @' + user.screen_name + ' no match!'
+	message += " Your closest match was " + closest 
+	message += "See whats left to find http://mmmbrain.com";
+  client.post('statuses/update', {status: message},  function(error, tweet, response){
+  });
+}
+
+function alreadyFound(user, winningUser) {
+	var message = '🐌🐌🐌 @' + user.screen_name + ' @'+winningUser.screen_name
+	message += ' already found that!'
+	message += "See whats left to find http://mmmbrain.com";
+  client.post('statuses/update', {status: message},  function(error, tweet, response){
+  });
+}
+	
+
 function onImageAnalysis(err, matches, tweet) {
 	console.log(matches);
-	//if (!err) {
-	//	Object.keys(matches).forEach(function(key){
-	//		if (matches[key] > 0.9) {
-	//			console.log("You've found " + key);
-	//		}
-	//	});
-	//}
+	if (!err) {
+		Object.keys(matches).forEach(function(key){
+			if (matches[key] > 0.9) {
+				console.log("You've found " + key);
+				tweetFound(tweet.user, key);
+			}
+		});
+	}
 }
 
 client.stream('statuses/filter', {track: process.env.TWITTER_BOT}, function(stream) {
@@ -31,7 +55,7 @@ client.stream('statuses/filter', {track: process.env.TWITTER_BOT}, function(stre
 				 'http://imagenet/upload',
 				 {formData: {upload: download}}, 
 				 function(err, httpResponse, body) {
-		         onImageAnalysis(err, body, tweet);
+		         onImageAnalysis(err, JSON.parse(body), tweet);
 				 }
   		 );
 		}
