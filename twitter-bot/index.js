1b56ee577e324bcd57912e84b2c80694299b9cfb
Jesse Vander Does <jesse@htmlfusion.com>
2015-12-19 15:59:52 -0800

    better error handling

--- twitter-bot/index.js
+++ twitter-bot/index.js
@@ -7,10 +7,10 @@ var _ = require('lodash'),
  database = new Database(dbPath);
  
 var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_TOKEN_SECRET
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+	access_token_secret: process.env.TWITTER_TOKEN_SECRET
 });
 
 function tweetFound(tweet, foundObject) {
@@ -18,25 +18,34 @@ function tweetFound(tweet, foundObject) {
 	foundObject.tweet = tweet;
 	database.update(collection, foundObject.id, foundObject);
 	var message = '🌟🌟🌟 @' + tweet.user.screen_name + ' you found a ' + foundObject.text + ". We're crossing it off the list http://mmmbrain.com";
-  client.post('statuses/update', {status: message},  function(error, tweet, response){
-  });
+	client.post('statuses/update', {status: message},  function(error, tweet, response){
+	});
 }
 
 function noMatch(user, closest) {
 	var message = '💣💣💣 @' + user.screen_name + ' no match!'
 	message += " Your closest match was " + closest + "."
 	message += " See whats left to find http://mmmbrain.com";
-  client.post('statuses/update', {status: message},  function(error, tweet, response){
-  });
+	client.post('statuses/update', {status: message},  function(error, tweet, response){
+	});
+}
+
+function onError(user) {
+	var message = '@' + user.screen_name + ' oh noes, something went wrong.'
+	message += ' Try a new photo, or just try again.'
+	message += " See whats left to find http://mmmbrain.com";
+	client.post('statuses/update', {status: message},  function(error, tweet, response){
+	});
 }
 
 function alreadyFound(user, winningUser) {
 	var message = '🐌🐌🐌 @' + user.screen_name + ' @'+winningUser.screen_name
 	message += ' already found that!'
 	message += " See whats left to find http://mmmbrain.com";
-  client.post('statuses/update', {status: message},  function(error, tweet, response){
-  });
+	client.post('statuses/update', {status: message},  function(error, tweet, response){
+	});
 }
+
 
 function bestMatch(matches) {
 	var best = _.max(_.pairs(matches), function(pair){return pair[1]})
@@ -56,18 +65,18 @@ function onImageAnalysis(err, matches, tweet) {
 			console.log("Best match " + text);
 			var foundObject = database.findOne(collection, {text: text});
 			console.log('found object', foundObject);
-      if (!Object.keys(foundObject).length){
+			if (!Object.keys(foundObject).length){
 				console.log("Invalid match");
 			} else if (!foundObject.found) {
 				console.log('found');
-			  tweetFound(tweet, foundObject);
+				tweetFound(tweet, foundObject);
 			} else {
 				console.log('already found');
 				alreadyFound(tweet.user, foundObject.tweet.user);
 			}
 
 		} else {
-		  console.log('no match');
+			console.log('no match');
 			noMatch(tweet.user, text);
 		}
 	}
@@ -76,26 +85,35 @@ function onImageAnalysis(err, matches, tweet) {
 client.stream('statuses/filter', {track: process.env.TWITTER_BOT}, function(stream) {
 
 	console.log('called');
-  stream.on('data', function(tweet) {
+	stream.on('data', function(tweet) {
 
 		if(tweet.entities.media) {
 			var mediaUrl = tweet.entities.media[0].media_url,
-      	download = request(mediaUrl);
+				download = request(mediaUrl);
 				
 			request.post(
 				 'http://imagenet/upload',
 				 {formData: {upload: download}}, 
 				 function(err, httpResponse, body) {
-		         onImageAnalysis(err, JSON.parse(body), tweet);
+						 try {
+							 var data = JSON.parse(body)
+							 if (data.error) {
+								 onError(tweet.user);
+							 } else {
+								 onImageAnalysis(err, data, tweet);
+							 }
+						 } catch(e) {
+							 onError(tweet.user);
+						 }
 				 }
-  		 );
+			 );
 		}
 		
 	});
  
-  stream.on('error', function(error) {
+	stream.on('error', function(error) {
 		console.log('error', error);
-  });
+	});
 
 });
 
